d1438148216e1deaeee77d22cbdc60c5cd4d4b76
MiranCZ <miraancz@gmail.com>
2026-05-28 17:25:10 +0200

    feat: Add more attribution items

--- app/src/main/java/me/miran/libreinfo/activity/attribution/AttributionActivity.kt
+++ app/src/main/java/me/miran/libreinfo/activity/attribution/AttributionActivity.kt
@@ -9,6 +9,8 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -26,7 +28,7 @@ class AttributionActivity : KBaseActivity(R.string.data_sources) {
     @Composable
     override fun CreateElements() {
         // TODO click into a dedicated screen with more detailed description or something
-        Column {
+        Column(Modifier.verticalScroll(rememberScrollState())) {
             AttributionItem(
                 "KORDIS JMK a.s.",
                 "Koordinace dopravní obslužnosti na území Jihomoravského kraje",
@@ -37,6 +39,24 @@ class AttributionActivity : KBaseActivity(R.string.data_sources) {
                 "Data Brno",
                 "Otevřená platforma sloužící ke sdílení dat o městě Brně",
                 R.drawable.data_brno_icon
+            )
+
+            AttributionItem(
+                "OpenStreetMap",
+                "Volně dostupná mapa světa vytvářená komunitou dobrovolníků",
+                R.drawable.openstreetmap_icon
+            )
+
+            AttributionItem(
+                "Font Awesome",
+                "Knihovna vektorových ikon a log",
+                R.drawable.fontawesome_icon
+            )
+
+            AttributionItem(
+                "Pfaedle",
+                "Nástroj pro přesné mapování tras veřejné dopravy",
+                R.drawable.pfaedle_icon
             )
         }
     }
