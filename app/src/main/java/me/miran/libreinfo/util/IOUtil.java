a81b96edf3044b982c32ae4a088d34ca237028f2
MiranCZ <miraancz@gmail.com>
2026-05-28 21:07:16 +0200

    chore: Use builtin for `readNBytes` if possible

--- app/src/main/java/me/miran/libreinfo/util/IOUtil.java
+++ app/src/main/java/me/miran/libreinfo/util/IOUtil.java
@@ -13,19 +13,23 @@ public class IOUtil {
     public static byte[] readAllBytes(InputStream stream) throws IOException {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
             return stream.readAllBytes();
-        } else {
-            try (ByteArrayOutputStream out = new ByteArrayOutputStream()) {
-                byte[] buffer = new byte[8192];
-                int n;
-                while ((n = stream.read(buffer)) != -1) {
-                    out.write(buffer, 0, n);
-                }
-                return out.toByteArray();
+        }
+
+        try (ByteArrayOutputStream out = new ByteArrayOutputStream()) {
+            byte[] buffer = new byte[8192];
+            int n;
+            while ((n = stream.read(buffer)) != -1) {
+                out.write(buffer, 0, n);
             }
+            return out.toByteArray();
         }
     }
 
     public static byte[] readNBytes(InputStream stream, int size) throws IOException {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            return stream.readNBytes(size);
+        }
+
         byte[] bytes = new byte[size];
         int read = stream.read(bytes);
 
