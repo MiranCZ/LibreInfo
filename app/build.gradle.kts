d395ac3c9a562e732bfba1e2e57b567b230caa6c
MiranCZ <miraancz@gmail.com>
2026-06-24 15:55:56 +0200

    Chore: Bump versions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,8 +34,8 @@ android {
         applicationId = "me.miran.libreinfo"
         minSdk = 26
         targetSdk = 36
-        versionCode = 1
-        versionName = "0.1"
+        versionCode = 2
+        versionName = "0.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
